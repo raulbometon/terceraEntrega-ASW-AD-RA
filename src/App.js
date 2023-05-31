50d0faece3b9151eb2094c987d71e5b1fd7182ef
raulbometon <raulbometon@gmail.com>
2023-05-31 23:22:17 +0200

    visual newissue

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-import IssuesList from './IssuesList';
-import IssueEdit from './IssueEdit';
+import IssuesList from "./IssuesList";
+import IssueEdit from "./IssueEdit";
+import NewIssue from "./NewIssue";
+import BulkInsert from "./BulkInsert";
 
 const App = () => {
   return (
     <Router>
       <div>
-        <Routes> {/* Use Routes instead of Switch */}
+        <Link to="/new">
+          {" "}
+          <button> + New Issue </button>{" "}
+        </Link>
+        <Link to="/bulk">
+          {" "}
+          <button> Bulk </button>{" "}
+        </Link>
+        <Routes>
+          {" "}
+          {/* Use Routes instead of Switch */}
           <Route path="/" element={<IssuesList />} />
           <Route path="/issues/:issueId" element={<IssueEdit />} />
+          <Route path="/new" element={<NewIssue />} />
+          <Route path="/bulk" element={<BulkInsert />} />
         </Routes>
       </div>
     </Router>
